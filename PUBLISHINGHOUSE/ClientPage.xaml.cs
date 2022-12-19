ad8ce7b1fb6735db999578d2a6126ffd9f618165
nfedov <stupid.dolbaeb@gmail.com>
2022-12-19 13:07:11 +0500

    kursach

---
+++ PUBLISHINGHOUSE/ClientPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace PUBLISHINGHOUSE
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientPage.xaml
+    /// </summary>
+    public partial class ClientPage : Page
+    {
+        public ClientPage()
+        {
+            InitializeComponent();
+            var db = new bdNefedovEntities();
+           // ClientGrid.ItemsSource = db.Connection.Client.ToList();
+            ClientGrid.ItemsSource = db.Book.ToList();
+            ClientGrid.ItemsSource = db.Genre.ToList();
+        }
+    }
+}
---
+++ PUBLISHINGHOUSE/ListBooksPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PUBLISHINGHOUSE
+{
+    /// <summary>
+    /// Логика взаимодействия для ListBooksPage.xaml
+    /// </summary>
+    public partial class ListBooksPage : Page
+    {
+        public ListBooksPage()
+        {
+            InitializeComponent();
+            var db = new bdNefedovEntities();
+           
+            BookGrid.ItemsSource = db.Book.ToList();
+            BookGrid.ItemsSource = db.Genre.ToList();
+        }
+    }
+}
